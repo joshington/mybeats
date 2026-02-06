e31ddeba738337f57f362580d08b95b95de8951b
joshington <bbosalj@gmail.com>
2026-02-06 12:09:57 +0300

    some deploy configs

--- mybeats/settings.py
+++ mybeats/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
